10a9c78c130f190c2804a1a544da0d3812f167f6
nguyenvancongdev <congthang.nuce@gmail.com>
2022-09-04 20:48:32 +0700

    deploy

--- deloy.bat
+++ deloy.bat
@@ -5,9 +5,9 @@ git commit -m "deploy"
 git push
 npm run build
 cd dist
+pause
 git init
 git add -A
 git commit -m "deploy"
 git push -f https://github.com/nguyenvancongdev/vuejs-demo.git master:gh-pages
-pause
 echo 'end'
