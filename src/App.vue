0090ef4c7aee4114ec0632a772da0bd20db4fb2c
nguyenvancongdev <congthang.nuce@gmail.com>
2022-09-05 14:20:48 +0700

    deploy

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
-  <router-link to="/">Home</router-link> |
+  <div>
+    <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+  </div>
  <router-view></router-view>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-
 
 export default {
   name: 'App',
-  // components: {
-  //   HelloWorld
-  // }
 }
 </script>
 
