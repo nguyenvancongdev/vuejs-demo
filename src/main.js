349ef75aba5af368b730b79ec02dc03382dd3bb9
nguyenvancongdev <congthang.nuce@gmail.com>
2022-10-11 21:41:22 +0700

    jjj

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import Vuex from 'vuex'
 
 loadFonts()
 
 createApp(App)
   .use(router)
   .use(vuetify)
+  .use(Vuex)
   .mount('#app')
