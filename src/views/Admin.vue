f0fc5f214c1e9b49b2129371893ac4c0054ab2ed
Nguyen Van Cong <congthang.nuce@gmail.com>
2022-10-27 09:38:09 +0000

    Update 3 files

---
+++ src/views/Admin.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="">
+     <v-btn>back to home </v-btn>
+    </div>
+
+ </template>
+ 
+ <script>
+ import { defineComponent } from 'vue';
+
+ 
+ export default defineComponent({
+   name: 'AdminScreen',
+
+ });
+ </script>
